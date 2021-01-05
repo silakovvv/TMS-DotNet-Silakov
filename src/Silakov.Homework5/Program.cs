468f8f8285670a8803616c4e678c14e611791de8
silakovvv <75270532+silakovvv@users.noreply.github.com>
2021-01-06 00:25:44 +0300

    init: add homework 5

---
+++ src/Silakov.Homework5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Silakov.Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
