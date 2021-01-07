b01cf057fe3732a18984433a43cad1a916b3c0cc
silakovvv <75270532+silakovvv@users.noreply.github.com>
2021-01-07 22:52:40 +0300

    init: add zoo manager and interfaces

---
+++ src/Silakov.Homework5/Interfaces/IZooManager.cs
@@ -0,0 +1,14 @@
+﻿using Silakov.Homework5.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silakov.Homework5.Interfaces
+{
+    public interface IZooManager
+    {
+        public List<AnimalBase> Animals { get; set; }
+
+        public void OutputInformation();
+    }
+}
---
+++ src/Silakov.Homework5/Managers/ZooManager.cs
@@ -0,0 +1,43 @@
+﻿using Silakov.Homework5.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silakov.Homework5.Managers
+{
+    class ZooManager
+    {
+        /// <summary>
+        /// Список животных зоопарка.
+        /// </summary>
+        private List<AnimalBase> Animals { get; set; }
+
+        /// <summary>
+        /// Базовый конструктор.
+        /// </summary>
+        public ZooManager()
+        {
+            Animals = new List<AnimalBase>();
+        }
+
+        /// <summary>
+        /// Добавление нового животного.
+        /// </summary>
+        /// <param name="animal"></param>
+        public void AddAnimal(AnimalBase animal)
+        {
+            Animals.Add(animal);
+        }
+
+        /// <summary>
+        /// Вывод всех животных.
+        /// </summary>
+        public void WalkingAroundZoo()
+        {
+            foreach (var animal in Animals)
+            {
+                animal.OutputInformation();
+            }
+        }
+    }
+}
