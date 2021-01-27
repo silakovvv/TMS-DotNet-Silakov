f18a2443481df5b1b728c9e77df7c105bc47703f
silakovvv <75270532+silakovvv@users.noreply.github.com>
2021-01-27 21:37:38 +0300

    init: add homework 7 - Fitness tracker

---
+++ src/Silakov.Homework7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Silakov.Homework7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Fitness Tracker...");
+            IEnumerable<Training> trainings = TrainingGenerator.GetTrainings();
+            Console.WriteLine("Started Fitness Tracker.");
+
+            var averageDistance = TrainingGenerator.GetAverageDistance(trainings);
+            var allTimeDistance = TrainingGenerator.GetAllTimeDistance(trainings);
+            var maxDistance = TrainingGenerator.GetMaxDistance(trainings);
+
+            Console.WriteLine($"Avarage distance for all trainings: {averageDistance}");
+            Console.WriteLine($"Max distance for all trainings: {maxDistance}");
+            Console.WriteLine($"Distance for all trainings: {allTimeDistance}");
+        }
+    }
+}
---
+++ src/Silakov.Homework7/Training.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silakov.Homework7
+{
+    public class Training
+    {
+        public TimeSpan Duration { get; set; }
+        public DateTime StartDate { get; set; }
+        public double Distance { get; set; }
+        public int Steps { get; set; }
+        public int AveragePulse { get; set; }
+    }
+}
